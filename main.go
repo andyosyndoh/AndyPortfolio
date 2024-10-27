59eafd349c786f7410cfdaf6b361e2375b066ae1
aosindo <andyovvo8@gmail.com>
2024-10-27 14:18:30 +0300

    refactor: handled main function

--- main.go
+++ main.go
@@ -1,15 +1,60 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
-	"text/template"
+	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		template, _ := template.ParseFiles("index.html")
-		template.Execute(w, nil)
+	mux := http.NewServeMux()
+	RegisterRoutes(mux)
+
+	wrappedMux := RouteChecker(mux)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: wrappedMux,
+	}
+
+	fmt.Println("server running @http://localhost:8080\n=====================================")
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error running server: ", err)
+	}
+}
+
+// RouteChecker is a middleware that checkes allowed routes
+func RouteChecker(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.URL.Path, "/static/") {
+			// Static(w,r)
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		if _, ok := allowedRoutes[r.URL.Path]; !ok {
+			NotFoundHandler(w, r)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func RegisterRoutes(mux *http.ServeMux) {
+	staticDir := GetProjectRoot("views", "static")
+	fs := http.FileServer(http.Dir(staticDir))
+	mux.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		HomeHandler(w, r)
 	})
 
-	http.ListenAndServe(":8080", nil)
+	mux.HandleFunc("/details", func(w http.ResponseWriter, r *http.Request) {
+		Details(w, r)
+	})
+
+	mux.HandleFunc("/api/search", func(w http.ResponseWriter, r *http.Request) {
+		SearchArtists(w, r)
+	})
 }
